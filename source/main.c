ae95f4c271e9f36c3c7259a46d3f1f902da17c1a
RetroGamer_74 <retrogamerdirecto@gmail.com>
2019-03-01 10:52:14 +0100

    Modified to use with ldm_mitm using less memory. Makefile updated to create in sd_card all files ready to be copied

--- source/main.c
+++ source/main.c
@@ -19,7 +19,7 @@
 #include "mp3.h"
 
 #define TITLE_ID 0x420000000000000E
-#define HEAP_SIZE 0x000540000
+#define HEAP_SIZE 0x000380000
 
 // we aren't an applet
 u32 __nx_applet_type = AppletType_None;
